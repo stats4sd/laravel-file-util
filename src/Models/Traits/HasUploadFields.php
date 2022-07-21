3c3e130194604ef966bf1f381040132c4b1962b7
dave-mills <dave-mills@users.noreply.github.com>
2022-07-21 08:16:59 +0000

    Fix styling

--- src/Models/Traits/HasUploadFields.php
+++ src/Models/Traits/HasUploadFields.php
@@ -91,7 +91,7 @@ trait HasUploadFields
      */
     public function uploadMultipleFilesWithNames($value, $attribute_name, $disk, $destination_path)
     {
-        if (!is_array($this->{$attribute_name})) {
+        if (! is_array($this->{$attribute_name})) {
             $attribute_value = json_decode($this->{$attribute_name}, true) ?? [];
         } else {
             $attribute_value = $this->{$attribute_name};
