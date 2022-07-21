6ebc7c2ae044d46c27f40ba7212085a012994b25
David Mills <d.e.mills@stats4sd.org>
2022-07-21 09:47:51 +0100

    swap pest for phpunit in example test

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -1,5 +1,16 @@
 <?php
 
-it('can test', function () {
-    expect(true)->toBeTrue();
-});
+namespace Stats4sd\FileUtil\Tests;
+
+use ExampleTestTwo;
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+
+    /** @test */
+    public function it_can_run_a_test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/Pest.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Stats4sd\FileUtil\Tests\TestCase;
-
-uses(TestCase::class)->in(__DIR__);
