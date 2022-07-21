89693be751ad841eae8fe2de9c3cc0cd997556d3
dave-mills <dave-mills@users.noreply.github.com>
2022-07-21 08:48:13 +0000

    Fix styling

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,12 +2,10 @@
 
 namespace Stats4sd\FileUtil\Tests;
 
-use ExampleTestTwo;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
 {
-
     /** @test */
     public function it_can_run_a_test(): void
     {
